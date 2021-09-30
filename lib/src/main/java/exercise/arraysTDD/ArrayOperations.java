46b96608c49d45d9607ab27c310635fb13a7c994
Michael <91134597+Weissi631@users.noreply.github.com>
2021-09-30 15:19:36 +0200

    submission - checkpoint 1

--- lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
+++ lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
@@ -1,94 +1,142 @@
 package exercise.arraysTDD;
 
+import java.util.Iterator;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
 public class ArrayOperations {
-    private final int[] numbers;      // array that needs to be sorted.
-    private final int[] unsorted;     // reference array which stays unsorted.
+	private final int[] numbers; // array that needs to be sorted.
+	private final int[] unsorted; // reference array which stays unsorted.
 
-    public ArrayOperations(int[] numbers) {
-        this.numbers = numbers;
-        this.unsorted = numbers.clone();
-    }
+	public ArrayOperations(int[] numbers) {
+		this.numbers = numbers;
+		this.unsorted = numbers.clone();
+	}
 
-    /* **********************************************************
-     *   Please implement the following methods.                 *
-     *   It is intended that they are not static.                *
-     * ***********************************************************/
+	/*
+	 * ********************************************************** Please implement
+	 * the following methods. * It is intended that they are not static. *
+	 ***********************************************************/
 
-    /**
-     * Prints out the numbers array.
-     */
+	/**
+	 * Prints out the numbers array.
+	 */
 
-    public void print() {
-        
-    }
+	public void print() {
+		for (int i = 0; i < numbers.length; i++) {
+			if (i == 0) {
+				System.out.print("[" + numbers[i] + ", ");
+			} else if (i == numbers.length - 1) {
+				System.out.println(numbers[i] + "]");
+			} else {
+				System.out.print(numbers[i] + ", ");
+			}
+		}
+	}
 
-    /**
-     * @return the sorted numbers array.
-     * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
-     */
-    public int[] sort() {
-       return null;
-    }
+	/**
+	 * @return the sorted numbers array.
+	 * @see <a
+	 *      href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
+	 */
+	public int[] sort() {
+		boolean sorted = false;
+		int temp = 0;
+		while (!sorted) {
+			for (int i = 0; i < numbers.length; i++) {
+				if (numbers[i] > numbers[i] +1)
+					temp = numbers[i];
+					numbers[i] = numbers[i+1];
+					numbers[i+1] = temp;.
+					sorted = false;
+			}
+		}
+		return null;
+	}
 
-    /**
-     * @return the sorted array in reverse order
-     */
-    public int[] revertSort() {
-        return null;
-    }
+	/**
+	 * @return the sorted array in reverse order
+	 */
+	public int[] revertSort() {
+		return null;
+	}
 
-    /**
-     * @return the unsorted array in reverted order.
-     */
-    public int[] reverted() {
-        return null;
-    }
+	/**
+	 * @return the unsorted array in reverted order.
+	 */
+	public int[] reverted() {
+		int[] reversed = new int[numbers.length];
+		int j = 0;
+		for (int i = numbers.length -1; i>=0; i--) {
+			reversed[j] = numbers[i];
+			j++;
+		}
+		return reversed;
+	}
 
-    /**
-     * @param value which should be searched for.
-     * @return true if the array contains the value, false otherwise.
-     */
-    public boolean contains(int value) {
-        return false;
-    }
+	/**
+	 * @param value which should be searched for.
+	 * @return true if the array contains the value, false otherwise.
+	 */
+	public boolean contains(int value) {
+		return false;
+	}
 
-    /**
-     * @return the average value of all elements summed up.
-     */
-    public double average() {
-        return 0.0;
-    }
+	/**
+	 * @return the average value of all elements summed up.
+	 */
+	public double average() {
+		return sumOfArray() / numbers.length;
+	}
 
-    /**
-     * @return the average value of all elements summed up, but without the highest and the lowest value.
-     */
-    public double trimmedMean() {
-        return 0.0;
-    }
+	/**
+	 * @return the average value of all elements summed up, but without the highest
+	 *         and the lowest value.
+	 */
+	public double trimmedMean() {
+		return (sumOfArray() - maxValue() - minValue()) / (numbers.length - 2);
+	}
 
-    /**
-     * @return the max value of the array. In the array [1,9,3] max would be 9.
-     */
-    public int maxValue() {
-        return 0;
-    }
+	/**
+	 * @return the max value of the array. In the array [1,9,3] max would be 9.
+	 */
+	public int maxValue() {
+		int max = numbers[0];
+		int i = 0;
+		for (i = 0; i < numbers.length; i++) {
+			if (numbers[i] > max) {
+				max = numbers[i];
+			}
+		}
+		return max;
+	}
 
-    /**
-     * @return the min value of the array. In the array [1,9,3] min would be 1.
-     */
-    public int minValue() {
-        return 0;
-    }
+	/**
+	 * @return the min value of the array. In the array [1,9,3] min would be 1.
+	 */
+	public int minValue() {
+		int min = numbers[0];
+		int i = 0;
+		for (i = 0; i < numbers.length; i++) {
+			if (numbers[i] < min) {
+				min = numbers[i];
+			}
+		}
+		return min;
+	}
 
-    /* **********************************************************
-     *   Feel free to add as many private helper methods as      *
-     *   you like.                                               *
-     * ***********************************************************/
+	/***********************************************************
+	 * 
+	 * Feel free to add as many private helper methods as * you like. *
+	 ***********************************************************/
 
-    private int someHelper(int[] tmp) {
-        return 1;
-    }
+	private double sumOfArray() {
+		double sum1 = 0;
+		int i = 0;
+		for (i = 0; i < numbers.length; i++) {
+			sum1 = sum1 + numbers[i];
+		}
+		return sum1;
+	}
 }
